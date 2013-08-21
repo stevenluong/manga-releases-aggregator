60a90b3fb8c3c316407f2179f4d5a0a483ae75b2
stevenluong <ste.luong@gmail.com>
2013-08-22 01:15:20 +0200

    connection to ROR

--- userManager.js
+++ userManager.js
@@ -3,24 +3,54 @@ var logger = new Logger();
 
 var Config = require("./config.js");
 var config = new Config();
+var https = require('https');
 
 var User = require("./user.js");
-
+var querystring = require('querystring');
 module.exports=function UserManager(){
 	this.getUsers=function(callback){
 		logger.trace("getUsers");
-		var users = new Array();
-		var user1 = new User(config.user1Email,config.selectedMangas);
-		var user2 = new User(config.user2Email,["Tower of God","Noblesse"]);
-		//var user3 = {
-		//	email:"user3",
-		//	releases: new Array(),
-		//	mangas:["Baby Steps","Tower of God","Noblesse"]};
-		users.push(user1);
-		users.push(user2);
-		//users.push(user3);
-		
-		callback(users);
+		var auth_token = "";
+		var options = {
+  hostname: config.rorHost,
+  port: 443,
+  path: config.rorRootPath+'/users.json?auth_token='+auth_token,
+  method: 'GET',
+	headers: {
+        'Content-Type': 'application/json',
+        //'Content-Length': data.length
+    }
+};
+logger.debug(options);
+var req = https.request(options, function(res) {
+  logger.info("statusCode: ");
+	logger.debug(res.statusCode);
+  logger.info("headers: ");
+	logger.debug(res.headers);
+
+  res.on('data', function(d) {
+		logger.debug(d);
+    //process.stdout.write(d);
+  });
+});
+req.end();
+
+req.on('error', function(e) {
+  console.error(e);
+});	
+//		https.get(config.rorRootPath+'/users.json', function(res) {
+//		console.log("statusCode: ", res.statusCode);
+//		console.log("headers: ", res.headers);
+//
+//		res.on('data', function(d) {
+//			process.stdout.write(d);
+//			var users = new Array()
+//			callback(users);
+//		});
+//
+//	}).on('error', function(e) {
+//		console.error(e);
+//	});		
 	}
 
 }
